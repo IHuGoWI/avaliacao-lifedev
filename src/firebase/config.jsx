acaba1e9f1acc46e6bf0a276a0e18f2a4bfb670b
ihugowi <orushugow@gmail.com>
2025-05-10 00:47:13 -0300

    -feat * Instânciação e  Configurações iniciais do firebase * Instalação do react-router-dom

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,21 +1,20 @@
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  apiKey: "AIzaSyBYVR9ChDdEkpE3JlSVuYzmKi1y_xAUP5U",
+  authDomain: "avaliacao-lifedev-hugo.firebaseapp.com",
+  projectId: "avaliacao-lifedev-hugo",
+  storageBucket: "avaliacao-lifedev-hugo.firebasestorage.app",
+  messagingSenderId: "591707868810",
+  appId: "1:591707868810:web:445e222a21f48f21396773",
 };
 
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-const db = getFirestore(app)
-const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db, auth }
+export { db, auth };
